73eacd1c75dbde4bb74622afbd1fdf93e6854e51
Aman Kumar <amankssm@gmail.com>
2017-12-30 13:25:27 +0530

    Welcome2

--- HelloMaven/src/test/java/com/javatpoint/Hello.java
+++ HelloMaven/src/test/java/com/javatpoint/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Hello world");
+		System.out.println("Welcome to Hello world NEW");
 	}
 
 }
